0259c29452d5b1c7a95d15a9b0bb9d2d77cbb26c
motake <munetoshi_otake@camp4.jp>
2022-02-21 16:53:41 +0900

    fix path

--- mytheme/inc/setup.php
+++ mytheme/inc/setup.php
@@ -15,17 +15,8 @@ add_action('after_setup_theme', function () {
     'gallery',
     'search-form',
   ]);
-  /*
-  add_theme_support('post-formats', [
-    'aside',
-    'gallery',
-    'link',
-    'image',
-    'quote',
-    'video',
-    'audio',
-  ]);
-  */
+  // add_theme_support('wp-block-styles');
+  // add_theme_support('editor-styles');
 
   // Register wp_nav_menu() menus
   // http://codex.wordpress.org/Function_Reference/register_nav_menus
@@ -35,11 +26,6 @@ add_action('after_setup_theme', function () {
   register_nav_menus([
     'footer_navigation_bottom' => __('Footer Navigation Bottom', THEME_DOMAIN),
   ]);
-
-  // Use main stylesheet for visual editor
-  // To add custom styles edit /dist/styles/layouts/_tinymce.scss
-  // add_theme_support('wp-block-styles');
-  add_theme_support('editor-styles');
 });
 
 /**
@@ -59,77 +45,33 @@ add_action(
       false,
       null
     );
-
     // main
-    wp_enqueue_style('main', asset_cache_buster('main.css'), false, null);
-    wp_enqueue_script('main', asset_cache_buster('main.js'), null, null, true);
+    wp_enqueue_style('main', cache_buster('dist/main.css'), false, null);
+    wp_enqueue_script('main', cache_buster('dist/main.js'), null, null, true);
   },
   100
 );
 
 /**
- * ビルドファイル用のキャッシュバスター
+ * テーマアセット用キャッシュバスター
+ *
+ * @param string $path テーマ内のパス
+ * @return string ハッシュ ID クエリ付きURL
  */
-function asset_cache_buster($path = '')
+function cache_buster($path = '')
 {
   if (!$path) {
     return '';
   }
 
-  $asset_path = ASSETS_DIR_PATH . $path;
-  $asset_uri = ASSETS_DIR_URI . $path;
+  $asset_path = get_template_directory() . $path;
+  $asset_uri = get_template_directory_uri() . $path;
   $hash_id = file_exists($asset_path)
     ? '?id=' . hash_file('fnv132', $asset_path)
     : '';
 
   return $asset_uri . $hash_id;
 }
-
-/**
- * Register sidebars
- */
-/*
-add_action('widgets_init', function () {
-  register_sidebar([
-    'name' => __('Primary', 'sage'),
-    'id' => 'sidebar-primary',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget' => '</section>',
-    'before_title' => '<h3>',
-    'after_title' => '</h3>',
-  ]);
-
-  register_sidebar([
-    'name' => __('Footer', 'sage'),
-    'id' => 'sidebar-footer',
-    'before_widget' => '<section class="widget %1$s %2$s">',
-    'after_widget' => '</section>',
-    'before_title' => '<h3>',
-    'after_title' => '</h3>',
-  ]);
-});
-*/
-
-/**
- * Determine which pages should NOT display the sidebar
- */
-/*
-function display_sidebar()
-{
-  static $display;
-
-  isset($display) ||
-    ($display = !in_array(false, [
-      // The sidebar will NOT be displayed if ANY of the following return true.
-      // @link https://codex.wordpress.org/Conditional_Tags
-      is_404(),
-      is_front_page(),
-      is_page_template('template-custom.php'),
-    ]));
-
-  return apply_filters('sage/display_sidebar', $display);
-}
-*/
 
 /**
  * remove wp_head meta
@@ -162,20 +104,6 @@ add_action(
   },
   102
 );
-
-/**
- * head 追加出力
- */
-/*
-add_action(
-  'wp_head',
-  function () {
-    // テーマカラー
-    // echo '<meta name="theme-color" content="#002978" />';
-  },
-  100
-);
- */
 
 /**
  * REST API から "user" endpoint を削除
@@ -321,15 +249,3 @@ add_filter(
   10,
   2
 );
-
-/**
- * スタイルインライン埋め込みフラグ
- *
- * @return boolean
- */
-/*
-function is_enable_inline_style()
-{
-  return !WP_DEBUG && strpos($_SERVER['HTTP_HOST'], 'localhost:') === false;
-}
-*/
