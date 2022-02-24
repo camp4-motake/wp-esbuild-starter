d00b4a74466036bfe776865e08c0eefb5eb6ca00
motake <munetoshi_otake@camp4.jp>
2022-02-24 10:30:08 +0900

    add env structure

--- mytheme/bs-config.js
+++ mytheme/bs-config.js
@@ -1,3 +1,8 @@
+const path = require('path');
+const envFile = path.resolve(process.cwd(), '../.env');
+require('dotenv').config({ path: envFile || null });
+const wpPort = process.env.LOCAL_SERVER_PORT || 8888;
+
 /**
  * Browsersync options
  * https://browsersync.io/docs/options
@@ -6,7 +11,7 @@ module.exports = {
   ui: false,
   files: ['resources/src', '**/*.{php,twig}'],
   ignore: ['node_modules'],
-  proxy: `http://localhost:${process.env.WP_ENV_PORT || 8888}`,
+  proxy: `http://localhost:${wpPort}`,
   ghostMode: false,
   open: false,
   notify: false,
