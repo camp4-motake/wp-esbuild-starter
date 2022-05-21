b4504b08d9c5ecbda4dd6d19f9b179f43b6d897e
motake <munetoshi_otake@camp4.jp>
2022-05-21 15:27:54 +0900

    add ts file

---
+++ mytheme/src/scripts/util/triggerEvent.ts
@@ -0,0 +1,17 @@
+/**
+ * カスタムイベントトリガー
+ */
+const triggerEvent = (element: HTMLElement, eventName: string, detail = {}) => {
+  let event;
+
+  try {
+    event = new CustomEvent(eventName, { detail });
+  } catch (e) {
+    // IE用fallback
+    event = document.createEvent('CustomEvent');
+    event.initCustomEvent(eventName, false, false, detail);
+  }
+
+  element.dispatchEvent(event);
+};
+export default triggerEvent;
