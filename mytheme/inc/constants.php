6436894fc598ea751ab8d26c6a9ab4f82732ce7a
motake <munetoshi_otake@camp4.jp>
2022-06-14 10:24:33 +0900

    fix uppercase

--- mytheme/inc/constants.php
+++ mytheme/inc/constants.php
@@ -20,7 +20,7 @@ define('MQ_SM', '(min-width:36em)');
 define('MQ_MD', '(min-width:48em)');
 define('MQ_LG', '(min-width:62em)');
 define('MQ_XL', '(min-width:75em)');
-define('MQ_2xl', '(min-width:100em)');
+define('MQ_2XL', '(min-width:100em)');
 define('MQ_LANDSCAPE', '(orientation: landscape)');
 define('MQ_PORTRAIT', '(orientation: portrait)');
 
