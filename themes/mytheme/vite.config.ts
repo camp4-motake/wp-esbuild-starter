8d7373cfe18d6d267def7a2970bb7e3636eb2b1c
motake <munetoshi_otake@camp4.jp>
2023-01-25 19:07:32 +0900

    remove deps

--- themes/mytheme/vite.config.ts
+++ themes/mytheme/vite.config.ts
@@ -2,19 +2,11 @@ import { existsSync } from "fs";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import FullReload from "vite-plugin-full-reload";
-import dotenv from "dotenv";
 
 const root = existsSync("../../package.json") ? "../../" : "./";
 const nodeModules = resolve(__dirname, root, "node_modules");
-const envFile = resolve(process.cwd(), "../../.env");
-// const port = { wp: process.env.LOCAL_SERVER_PORT || 8888 };
 
-dotenv.config({ path: envFile || undefined });
-
-/**
- * vite config
- * @see https://ja.vitejs.dev/config/
- */
+// https://ja.vitejs.dev/config/
 export default defineConfig({
   plugins: [FullReload(["**/*.{php,twig}", "theme.json"])],
   // logLevel: "warn",
