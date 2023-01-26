0675c104e4a8e56e951cc0d3fe282d082e7786ff
motake <munetoshi_otake@camp4.jp>
2023-01-26 16:42:15 +0900

    add server open

--- themes/mytheme/vite.config.ts
+++ themes/mytheme/vite.config.ts
@@ -2,15 +2,27 @@ import { existsSync } from "fs";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import FullReload from "vite-plugin-full-reload";
+import dotenv from "dotenv";
 
 const root = existsSync("../../package.json") ? "../../" : "./";
 const nodeModules = resolve(__dirname, root, "node_modules");
+const envFile = resolve(process.cwd(), "../../.env");
+dotenv.config({ path: envFile || undefined });
+
+const { LOCAL_URL, LOCAL_SERVER_PORT, IS_OPEN } = process.env;
+let open: string | boolean = false;
+
+if (IS_OPEN !== "false") {
+  open =
+    LOCAL_URL ||
+    (LOCAL_SERVER_PORT ? `http://localhost:${LOCAL_SERVER_PORT}` : false);
+}
 
 // https://ja.vitejs.dev/config/
 export default defineConfig({
   plugins: [FullReload(["**/*.{php,twig}", "theme.json"])],
   // logLevel: "warn",
-  server: { strictPort: true },
+  server: { strictPort: true, open },
   base: "./",
   publicDir: false,
   cacheDir: resolve(nodeModules, "./.vite"),
