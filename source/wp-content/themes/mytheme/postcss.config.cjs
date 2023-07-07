c1819dbfb04fce79e8ba8ce6ae9435c4e32fe63a
motake <munetoshi_otake@camp4.jp>
2023-07-07 15:22:06 +0900

    change import

--- source/wp-content/themes/mytheme/postcss.config.cjs
+++ source/wp-content/themes/mytheme/postcss.config.cjs
@@ -2,14 +2,11 @@ const inProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
   plugins: {
-    "postcss-import": {},
+    "postcss-easy-import": {},
     "postcss-preset-env": {
       stage: 2,
-      autoprefixer: { grid: "autoplace" },
-      features: {
-        "custom-properties": false,
-        "nesting-rules": true,
-      },
+      autoprefixer: {},
+      features: { "nesting-rules": true },
     },
     "postcss-url": { filter: "**/_inline/*", url: "inline" },
     "postcss-sort-media-queries": {},
