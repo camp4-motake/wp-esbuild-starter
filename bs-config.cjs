74ef71d2e297bd711ef3de239401dadbbdfc1208
motake <munetoshi_otake@camp4.jp>
2023-07-14 13:55:17 +0900

    change bs structure

---
+++ bs-config.cjs
@@ -0,0 +1,18 @@
+const { resolve } = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: resolve(__dirname, "./.env") });
+
+const { PROXY_URL, WP_PORT, WP_THEME_NAME } = process.env;
+const themePath = `source/wp-content/themes/${WP_THEME_NAME}`;
+
+module.exports = {
+  files: [`**/*.php`, `dist/**/*`, `theme.json`].map((p) =>
+    resolve(themePath, p),
+  ),
+  proxy: PROXY_URL || `http://localhost:${WP_PORT}`,
+  ghostMode: false,
+  notify: false,
+  open: false,
+  ui: false,
+};
