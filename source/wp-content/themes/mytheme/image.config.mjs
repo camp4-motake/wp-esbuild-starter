2d102b9f0debb7ca1620f2f0afcff5c88d541404
motake <munetoshi_otake@camp4.jp>
2023-09-15 17:34:51 +0900

    watcher

--- source/wp-content/themes/mytheme/image.config.mjs
+++ source/wp-content/themes/mytheme/image.config.mjs
@@ -2,19 +2,35 @@ import path from "path";
 import findWorkspaceRoot from "find-yarn-workspace-root";
 import { fileURLToPath } from "url";
 import ImgMin from "./src/_lib/ImgMin.mjs";
+import chokidar from "chokidar";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const root = findWorkspaceRoot(__dirname) || "./";
+const inProduction = process.env.NODE_ENV === "production";
 
 /**
  * compress image & generate webp
  */
-const imgTask = new ImgMin({
-  src: ["./src/**/*.{jpg,jpeg,png,svg}", "!./src/**/_*/**"],
-  dest: "./dist",
-  option: {
-    base: "./src",
-    cacheDir: path.resolve(root, `node_modules/.cache/images`),
-  },
-});
-await imgTask.run();
+const imgTask = async (src) => {
+  const img = new ImgMin({
+    src,
+    dest: "./dist",
+    option: {
+      base: "./src",
+      cacheDir: path.resolve(root, `node_modules/.cache/images`),
+    },
+  });
+  return await img.run();
+};
+await imgTask(["./src/**/*.{jpg,jpeg,png,svg}", "!./src/**/_*/**"]);
+
+// watch
+if (!inProduction) {
+  const watcher = chokidar.watch("./src/images", {
+    ignored: [".*", "**/_*/**"],
+    persistent: true,
+  });
+  watcher
+    .on("add", async (path) => await imgTask(path))
+    .on("change", async (path) => await imgTask(path));
+}
