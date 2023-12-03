27db9cddabaea583c5e7484604387dddfded060f
motake <munetoshi_otake@camp4.jp>
2023-12-03 18:30:53 +0900

    remove

--- source/wp-content/themes/mytheme/templates/components/content-item-news-card.php
+++
@@ -1,7 +0,0 @@
-<a href="<?php the_permalink(); ?>" class="card">
-  <figure class="card-thumbnail"><?php the_post_thumbnail(); ?></figure>
-  <div class="card-content">
-    <h3 class="card-heading"><?php the_title(); ?></h3>
-    <?php get_template_part('templates/components/entry-meta'); ?>
-  </div>
-</a>
--- source/wp-content/themes/mytheme/templates/components/content-item-news-media.php
+++
@@ -1,8 +0,0 @@
-<a href="<?php the_permalink(); ?>" class="media">
-  <figure class="media-thumbnail"><?php the_post_thumbnail(); ?></figure>
-  <div class="media-content">
-    <div class="media-category"><?php get_template_part('templates/components/badge-loop'); ?></div>
-    <h2 class="media-heading"><?php the_title(); ?></h2>
-    <?php get_template_part('templates/components/entry-meta'); ?>
-  </div>
-</a>
