f0fc09b12d1fd4db6c6da4e99370b96bad63f7ef
motake <munetoshi_otake@camp4.jp>
2023-12-04 10:00:22 +0900

    update scripts

--- bs-config.cjs
+++ bs-config.cjs
@@ -1,8 +1,4 @@
 const { resolve } = require("path")
-const dotenv = require("dotenv")
-
-dotenv.config({ path: resolve(__dirname, "./.env") })
-
 const { PROXY_URL, WP_PORT, WP_THEME_NAME } = process.env
 const themePath = `source/wp-content/themes/${WP_THEME_NAME}`
 
--- source/wp-content/themes/mytheme/postcss.config.cjs
+++ source/wp-content/themes/mytheme/postcss.config.cjs
@@ -5,12 +5,11 @@ module.exports = (ctx) => {
     map: ctx.options.map,
     plugins: {
       "postcss-easy-import": {},
+      "postcss-url": { filter: "**/_inline/*", url: "inline" },
       "postcss-preset-env": {
         stage: 2,
-        autoprefixer: {},
         features: { "nesting-rules": true, "custom-properties": false },
       },
-      "postcss-url": { filter: "**/_inline/*", url: "inline" },
       "postcss-sort-media-queries": {},
       ...(inProduction
         ? { cssnano: { preset: ["default", { calc: false }] } }
