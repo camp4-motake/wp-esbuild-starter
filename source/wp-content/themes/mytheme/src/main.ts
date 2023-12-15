54ecee72acffd37462d6bcd0994e27574bdf4c97
motake <munetoshi_otake@camp4.jp>
2023-12-15 11:22:00 +0900

    housekeeping

--- source/wp-content/themes/mytheme/src/main.ts
+++ source/wp-content/themes/mytheme/src/main.ts
@@ -1,8 +1,8 @@
-import "./scripts"
+import intersect from "@alpinejs/intersect"
+import Alpine from "alpinejs"
 
 /**
- * WORKAROUND:
- * vite css glob import
+ * WORKAROUND: main css glob import
  * @see https://github.com/vitejs/vite/discussions/6688#discussioncomment-3649999
  */
 import.meta.glob("./styles/config/**/*.css", { eager: true })
@@ -11,3 +11,17 @@ import.meta.glob(
   "./styles/{global,plugins,blocks,components,partials,utils,routes}/**/*.css",
   { eager: true },
 )
+
+/**
+ * init alpine.js assets
+ * @see https://alpinejs.dev/start-here
+ */
+const components = import.meta.glob("./scripts/components/**/*.{js,ts,jsx,tsx}")
+const stores = import.meta.glob("./scripts/stores/**/*.{js,ts,jsx,tsx}")
+
+window.Alpine = Alpine
+Alpine.plugin(intersect)
+await Promise.all(
+  [...Object.values(components), ...Object.values(stores)].map((mod) => mod()),
+)
+Alpine.start()
