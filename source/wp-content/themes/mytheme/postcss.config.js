b67f15cc4521baa203db1b6941866c4426e61822
motake <munetoshi_otake@camp4.jp>
2023-12-15 15:42:00 +0900

    add deps

--- source/wp-content/themes/mytheme/postcss.config.js
+++ source/wp-content/themes/mytheme/postcss.config.js
@@ -1,5 +1,9 @@
 export default () => ({
   plugins: {
+    // WORKAROUND https://github.com/csstools/postcss-plugins/tree/main/plugins/postcss-global-data#readme
+    "@csstools/postcss-global-data": {
+      files: ["./src/styles/config/custom-media.css"],
+    },
     "postcss-url": { filter: "**/_inline/*", url: "inline" },
     "postcss-preset-env": { stage: 2, features: { "nesting-rules": true } },
     "postcss-sort-media-queries": {},
