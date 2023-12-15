16862615e0188f493e7f5d270827c75c8763ae2b
motake <munetoshi_otake@camp4.jp>
2023-12-15 17:11:05 +0900

    remove deps

--- source/wp-content/themes/mytheme/postcss.config.js
+++ source/wp-content/themes/mytheme/postcss.config.js
@@ -3,7 +3,6 @@ export default () => ({
     "@csstools/postcss-global-data": {
       files: ["./src/styles/config/custom-media.css"],
     },
-    "postcss-url": { filter: "**/_inline/*", url: "inline" },
     "postcss-preset-env": { stage: 2, features: { "nesting-rules": true } },
     "postcss-sort-media-queries": {},
   },
