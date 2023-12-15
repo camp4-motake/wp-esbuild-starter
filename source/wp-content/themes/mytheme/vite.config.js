34fb6bc8e73ecfbae6ba03a1a3b86aa2b4567166
motake <munetoshi_otake@camp4.jp>
2023-12-15 16:41:03 +0900

    update config

--- source/wp-content/themes/mytheme/vite.config.js
+++ source/wp-content/themes/mytheme/vite.config.js
@@ -5,23 +5,8 @@ import { pluginImage, pluginReload } from "./src/_lib/vitePlugins.js"
  * @see https://ja.vitejs.dev/config/
  */
 export default defineConfig(({ mode }) => {
-  const { VITE_SERVER_OPEN, WP_PORT, WP_ENTRY } = process.env
-
-  /**
-   * "editor" asset config (build only)
-   */
-  if (mode === "production" && WP_ENTRY === "editor") {
-    return {
-      publicDir: "src/static",
-      build: {
-        outDir: "dist",
-        assetsDir: "",
-        emptyOutDir: false,
-        manifest: ".vite/manifest.editor.json",
-        rollupOptions: { input: { editor: "src/editor.ts" } },
-      },
-    }
-  }
+  const { VITE_SERVER_OPEN, WP_PORT, TARGET } = process.env
+  const prefix = TARGET || "main"
 
   /**
    * "main" assets config
@@ -31,9 +16,12 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: "dist",
       assetsDir: "",
-      emptyOutDir: true,
-      manifest: ".vite/manifest.main.json",
-      rollupOptions: { input: { main: "src/main.ts" } },
+      emptyOutDir: false,
+      manifest: `.vite/manifest.${prefix}.json`,
+      rollupOptions: {
+        input: { [prefix]: `src/${prefix}.ts` },
+        output: { chunkFileNames: `chunk.[hash].js` },
+      },
     },
 
     server: { open: VITE_SERVER_OPEN || `http://localhost:${WP_PORT}` },
