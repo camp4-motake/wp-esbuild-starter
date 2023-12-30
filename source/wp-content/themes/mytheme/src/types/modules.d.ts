9d739989d1757e2513b30a9230e7f126dee515fd
motake <munetoshi_otake@camp4.jp>
2023-12-30 14:42:43 +0900

    remove unused types

--- source/wp-content/themes/mytheme/src/types/modules.d.ts
+++ source/wp-content/themes/mytheme/src/types/modules.d.ts
@@ -2,13 +2,3 @@ declare module "@alpinejs/collapse"
 declare module "@alpinejs/intersect"
 declare module "@alpinejs/persist"
 declare module "splitting"
-
-declare module "*.data.ts" {
-  const value: () => void
-  export default value
-}
-
-declare module "*.store.ts" {
-  const value: () => void
-  export default value
-}
